9816660747a9c2c6845f24492e47d063441bf44a
jimmely <dfjimmy.li@mail.utoronto.ca>
2017-09-22 17:07:37 -0400

    thing

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print "hello world!"
+from bottle import route, template, run
+
+
+@route('/hello')
+def helloagain(name='World'):
+    return template('helloworld', hi=name)
+
+# print bottle.TEMPLATE_PATH
+run(host='localhost', port=8000, debug=True)
