e529a15986f32e59fdf9d4c27f53c421604fc89b
Patricia <patsmrkt@gmail.com>
2017-09-30 13:46:04 -0400

    added static path for css files

--- main-endpoints.py
+++ main-endpoints.py
@@ -6,4 +6,8 @@ TEMPLATE_PATH.insert(0,'/views/css');
 def root_path():
     return template('index')
 
+@get('/static/css/<filepath:re:.*\.css>')
+def static(filepath):
+    return static_file(filepath, root='static/css')
+
 run(host='localhost', port=8000, debug=True);
