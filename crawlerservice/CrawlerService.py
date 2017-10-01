3919b4e364c61c49d071664b2c037849093de67b
jimmely <dfjimmy.li@gmail.com>
2017-09-30 23:34:38 -0400

    reformatted code and added some comments

--- crawlerservice/CrawlerService.py
+++ crawlerservice/CrawlerService.py
@@ -7,29 +7,31 @@ from WebScrape import *;
 class CrawlerService():
     
     def __init__(self):
+        # instantiates data / service classes
         self.textData = TextUrlData();
         self.webscraper = WebScrape(self.textData);
         self.crawler = Crawler(self.textData); 
         
+        # loads all data structures by calling lower tier helper classes
         self.generate_docId_to_url();
-        self.webscraper.scrape_the_web();
+        self.generate_webscraping_datastructures();
         self.get_resolved_inverted_index();
     
     def generate_docId_to_url(self):
         docId_to_url = self.textData.getDocId_to_url();
         self.__read_input_file_helper(docId_to_url); 
-        
+        return
+    
+    def generate_webscraping_datastructures(self):
+        self.webscraper.scrape_the_web();
         return
     
     def get_resolved_inverted_index(self): 
-#         print self.crawler.get_resolved_inverted_index();
-        
-        return
+        inverted_index = self.crawler.get_resolved_inverted_index();
+        return inverted_index;
         
     # Private Helper Functions
-    
     # Reads input file from base path of application
-    # @Return     
     def __read_input_file_helper(self, docId_to_url):
         with open('input.txt') as inputfile:
             for line in inputfile:
