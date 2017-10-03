8a2bc023635e8134db75b31fc573143a895f58d6
jimmely <dfjimmy.li@gmail.com>
2017-10-03 18:15:52 -0400

    refactor CrawlerServices to WebScrapingServices

--- main-endpoints.py
+++ main-endpoints.py
@@ -1,5 +1,5 @@
 from bottle import *
-from crawlerservice.CrawlerService import *
+from WebScrapingServices.CrawlerService import *
 from toptwenty.toptwenty import TopTwenty
 from toptwenty.word_data import WordData
 from HTMLFormatter.html_helper import results_html
@@ -14,9 +14,11 @@ def root_path():
     else:
         return results_html(request.query['keywords'].lower(), mostPopular);
 
-
 @get('/static/css/<filepath:re:.*\.css>')
 def static(filepath):
     return static_file(filepath, root='static/css')
 
-run(host='localhost', port=8000, debug=True);
+
+if __name__ == '__main__':
+    run(host='localhost', port=8000, debug=True);
+
