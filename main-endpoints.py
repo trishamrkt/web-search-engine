cfad26418a2c686ecaeb9321df6f4338e177e445
Patricia <patsmrkt@gmail.com>
2017-10-03 18:09:16 -0400

    move word_data() to general HTMLFormatter folder

--- main-endpoints.py
+++ main-endpoints.py
@@ -2,29 +2,17 @@ from bottle import *
 from crawlerservice.CrawlerService import *
 from toptwenty.toptwenty import TopTwenty
 from toptwenty.word_data import WordData
+from HTMLFormatter.html_helper import results_html
 
 crawlerService = CrawlerService();
-most_popular = TopTwenty();
-
-# Returns HTML for results page 
-def word_count():
-    search_string = request.query['keywords'].lower();
-    word_data = WordData();
-
-    # Gets HTML for table with words and their word counts
-    html = '<link type="text/css" rel="stylesheet" href="/static/css/word_table_data.css"\>'
-    html += "<link href='https://fonts.googleapis.com/css?family=Assistant' rel='stylesheet'>"
-    html += '<nav class="navi"><a href="/">Googao</a></nav>'
-    html = html + word_data.get_table_html(search_string.strip(), most_popular);
-    html = html + most_popular.get_table_html();
-    return html;
+mostPopular = TopTwenty();
 
 @route('/')
 def root_path():
     if request.query_string == '':
         return template('index')
     else:
-        return word_count();
+        return results_html(request.query['keywords'].lower(), mostPopular);
 
 
 @get('/static/css/<filepath:re:.*\.css>')
