c7d163712ab48bffc0ada95e8ba875f8846467db
Patricia <patsmrkt@gmail.com>
2017-10-05 21:09:42 -0400

    Comment code

--- UnitTests/ResultsPageServicesTest.py
+++ UnitTests/ResultsPageServicesTest.py
@@ -13,6 +13,8 @@ class ResultsPageServicesTest(unittest.TestCase):
         return
 
     # Test template for testing results table
+    # Params - test_string - represent search query
+    #        - expected values in results table returned as a dict
     def results_test_template(self, test_string, expected):
         # Get result from functions we wrote
         self.word_data.add_words(test_string);
@@ -55,7 +57,7 @@ class ResultsPageServicesTest(unittest.TestCase):
 
     # Test with extra white space at beginning, middle, and end of string
     def test_results_four(self):
-        test_string = "     hello world      !  "
+        test_string = "     hello    world      !  "
         expected_result = {'hello' : 1, 'world' : 1, '!' : 1}
         self.results_test_template(test_string, expected_result)
         return
@@ -67,7 +69,10 @@ class ResultsPageServicesTest(unittest.TestCase):
         self.results_test_template(test_string, expected_result)
         return
 
+    # ------- TESTS FOR SEARCH HISTORY DATA ------- #
     # Test template for testing history
+    # Params - list of strings - represent separate search queries
+    #        - expected return value of dict
     def history_test_template(self, strings, expected):
         for string in strings:
             temp_data = WordData();
@@ -87,10 +92,11 @@ class ResultsPageServicesTest(unittest.TestCase):
         return
 
     # > 20 unique word inputs - only the top 20 most frequent are in expected dict
-    # 1 only displayed onces, 2-21 displayed > 1 so they should be in the dict
+    # '1' only displayed once, 2-21 displayed > 1 times so they should be in the dict
     def test_history_two(self):
         test_strings = ['1', '2 2', '3 3', '4 4', '5 5', '6 6', '7 7', '8 8', '9 9', '10 10', '11 11',
                         '12 12', '13 13', '14 14', '15 15', '16 16', '17 17', '18 18', '19 19', '20', '20', '20 20 20 20 20 20', '21 21']
+
         expected_result = {'2' : 2, '3' : 2, '4' : 2, '5' : 2, '6' : 2, '7' : 2, '8' : 2, '9' : 2, '10' : 2, '11' : 2,
                         '12' : 2, '13' : 2, '14' : 2, '15' : 2, '16' : 2, '17' : 2, '18' : 2, '19' : 2, '20': 8, '21' : 2}
         self.history_test_template(test_strings, expected_result);
