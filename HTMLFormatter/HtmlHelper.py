5825cf21ac3f43a573e02d25d9c3df6078bf5a8b
Patricia <patsmrkt@gmail.com>
2017-10-21 22:59:30 -0400

    FRONT end functionality
    - Signed in Mode: 10 most recent, top twenty
    - Anonymous mode: word count

--- HTMLFormatter/HtmlHelper.py
+++ HTMLFormatter/HtmlHelper.py
@@ -1,14 +1,35 @@
 from ResultsPageServices.WordData import WordData
+from ResultsPageServices.TopTwenty import TopTwenty
 
-# Returns HTML for results page
-def results_html(searchString, mostPopular):
-    wordData = WordData();
-    wordData.add_words(searchString)
+# Declare constants for HTML 
+STYLE = '<link type="text/css" rel="stylesheet" href="/static/css/word_table_data.css"\>'
+FONTS = '<link href="https://fonts.googleapis.com/css?family=Assistant" rel="stylesheet" >'
+NAV = '<nav class="navi"><a href="/">Googao</a></nav>'
 
-    # Gets HTML for table with words and their word counts
-    html = '<link type="text/css" rel="stylesheet" href="/static/css/word_table_data.css"\>'
-    html += "<link href='https://fonts.googleapis.com/css?family=Assistant' rel='stylesheet'>"
-    html += '<nav class="navi"><a href="/">Googao</a></nav>'
-    html = html + wordData.get_table_html(searchString.strip(), mostPopular);
-    html = html + mostPopular.get_table_html();
+# Return HTML for Anonymous mode
+def anonymous_results(search_string):
+    word_data = WordData();
+    word_data.add_words(search_string);
+    table_html = word_data.get_table_html(search_string)
+    html = STYLE + FONTS + NAV + table_html;
     return html;
+
+# Return HTML for Signed In Mode
+def signed_in_results(search_string, history, most_recent):
+    word_data = WordData();
+    top_twenty = TopTwenty()
+    word_data.add_words(search_string);
+
+    top_twenty.set_top(history);
+    top_twenty.set_searched(most_recent)
+
+    # Get HTML for the results page
+    word_count_table_html = word_data.get_table_html(search_string.strip(), top_twenty);
+    top_twenty_table_html = top_twenty.get_popular_table_html();
+    most_recent_table_html = top_twenty.get_searched_table_html();
+    html = STYLE + FONTS + NAV + word_count_table_html + top_twenty_table_html + most_recent_table_html;
+
+    top_twenty_data = top_twenty.get_top();
+    most_recent_data = top_twenty.get_searched();
+
+    return [html, top_twenty_data, most_recent_data];
--- SessionManagement/User.py
+++ SessionManagement/User.py
@@ -1,20 +1,28 @@
+from collections import OrderedDict
 
 class User():
-    
+
     def __init__(self):
-        self.__history = [];
+        self.__history = {};
+        self.__most_recent = {};
         self.__first_name = '';
         self.__last_name = '';
-        
-    
+
+
     def getHistory(self):
         return self.__history;
-    
+
+    def getMostRecent(self):
+        return self.__most_recent;
+
     def getUserInfo(self):
         return self.__user_info;
-    
+
     def setHistory(self, history):
         self.__history = history;
-        
+
+    def setMostRecent(self, most_recent):
+        self.__most_recent = most_recent;
+
     def setUserInfo(self, user_info):
         self.__user_info = user_info;
