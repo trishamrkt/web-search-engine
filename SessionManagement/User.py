fc2f3fc3d7df79a8d9b15a07b6475746efc11e66
jimmy1231 <dfjimmy.li@gmail.com>
2017-10-21 20:32:22 -0400

    added some boilerplate code for session manager

---
+++ SessionManagement/User.py
@@ -0,0 +1,8 @@
+
+class User():
+    
+    def __init__(self):
+        self.__
+    
+    
+    
---
+++ SessionManagement/UserSessionManager.py
@@ -0,0 +1,11 @@
+
+class UserSessionManager():
+    
+    def __init__(self):
+        self._active_sessions = {};
+        
+    
+    def getUserBySessionId(self, sessionId):
+        
+    
+        return None;
